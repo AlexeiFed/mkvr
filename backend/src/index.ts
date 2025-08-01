adf0db45baf546a796201a66ae9a280f4a690c23
Alex <23alex08@gmail.com>
2025-08-01 23:01:02 +1000

    исправления опять

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -74,8 +74,7 @@ const corsOrigins = process.env['CORS_ORIGIN']
         'http://localhost:5174',
         'https://alexeymkvr.github.io',
         'https://alexeymkvr.github.io/MKVR',
-        'https://alexeifed.github.io',
-        'https://alexeifed.github.io/MKVR'
+        'https://alexeifed.github.io'
     ];
 
 app.use(cors({
